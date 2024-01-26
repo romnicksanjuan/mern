b9e131ffc4118eb4d0f5cca589cf6be604fd3be4
romnick <romnicksanjuan9@gmail.com>
2024-01-26 23:58:34 +0800

    sdssa

--- client/src/productPages/Create.jsx
+++ client/src/productPages/Create.jsx
@@ -8,66 +8,68 @@ import NavBar from '../navigation/nav';
 
 
 function CreateProduct() {
-  const [title , setTitle] = useState();
-  const [price , setPrice] = useState();
-  const [selected , setSelectedFiles] = useState(null);
-//   const navigate = useNavigate();
+  const [title, setTitle] = useState();
+  const [price, setPrice] = useState();
+  const [selected, setSelectedFiles] = useState(null);
+  //   const navigate = useNavigate();
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const formData = new FormData();
-  formData.append('title', title)
-  formData.append('price', price)
-  formData.append('file', selected)
-  
-  saveProduct();
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-const saveProduct = async () =>{
 
-  try {
-    const response = await axios.post('https://mern-server-inky.vercel.app/create', formData)
-    console.log(response.data)
-  } catch (error) {
-    console.log(error)
+    saveProduct();
   }
 
-}
+  const saveProduct = async () => {
+
+    try {
+      const formData = new FormData();
+      formData.append('title', title)
+      formData.append('price', price)
+      formData.append('file', selected)
+      
+      const response = await axios.post('https://mern-server-inky.vercel.app/create', formData)
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
 
 
   return (
 
     <>
-    {/* <NavBar/> */}
+      {/* <NavBar/> */}
       <form onSubmit={handleSubmit}>
-      <h2>Create</h2>
+        <h2>Create</h2>
 
-      <div className='mb-3'>
-        <label htmlFor="title">
-        <strong>Title:</strong>
-        </label>
-        <input type="text" name="title" id="title" autoComplete='off' placeholder='enter your title' className='form-control rounded-0' onChange={(e) => setTitle(e.target.value)}/>
-      </div>
+        <div className='mb-3'>
+          <label htmlFor="title">
+            <strong>Title:</strong>
+          </label>
+          <input type="text" name="title" id="title" autoComplete='off' placeholder='enter your title' className='form-control rounded-0' onChange={(e) => setTitle(e.target.value)} />
+        </div>
 
-      <div className='mb-3'>
-        <label htmlFor="title">
-        <strong>Image:</strong>
-        </label>
-        <input type="file" name="file"  className='form-control rounded-0' onChange={(e) => setSelectedFiles(e.target.files[0])}/>
-      </div>
+        <div className='mb-3'>
+          <label htmlFor="title">
+            <strong>Image:</strong>
+          </label>
+          <input type="file" name="file" className='form-control rounded-0' onChange={(e) => setSelectedFiles(e.target.files[0])} />
+        </div>
 
-      <div className='mb-3'>
-        <label htmlFor="price">
-        <strong>Price:</strong>
-        </label>
-        <input type="number" name="price" id="price" autoComplete='off' placeholder='enter your title' className='form-control rounded-0' onChange={(e) => setPrice(e.target.value)}/>
-      </div>
+        <div className='mb-3'>
+          <label htmlFor="price">
+            <strong>Price:</strong>
+          </label>
+          <input type="number" name="price" id="price" autoComplete='off' placeholder='enter your title' className='form-control rounded-0' onChange={(e) => setPrice(e.target.value)} />
+        </div>
 
-  <button type='submit' className='btn btn-success w-100 rounded-0'>Create</button>
-  
-  </form>
-  </>
-    
+        <button type='submit' className='btn btn-success w-100 rounded-0'>Create</button>
+
+      </form>
+    </>
+
   )
 }
 
