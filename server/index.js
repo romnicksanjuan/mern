605015b4afb4b2f97bd0bfef16e83d6b8d80bbe1
romnick <romnicksanjuan9@gmail.com>
2024-01-27 15:46:33 +0800

    ll

--- server/index.js
+++ server/index.js
@@ -69,9 +69,9 @@ app.post('/login', async (req, res) => {
 
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads/")
-    },
+    // destination: (req, file, cb) => {
+    //     cb(null, "uploads/")
+    // },
     filename: function (req, file, cb) {
         cb(null, file.originalname); // Set the filename
     }
